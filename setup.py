824b3180bb35448cee231efd630c75a5a52e61b8
julianaIUD <juliana.pena@iudigital.edu.co>
2025-05-10 04:07:03 -0500

    Carga inicial

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="edu_pad",
+    version="0.0.1",
+    author="Juliana Pena",
+    author_email="juliana.pena@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad1","actividad2"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "beautifulsoup4"
+    ]
+)
